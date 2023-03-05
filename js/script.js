743b44b21047e67f07a7084a45af0ea254d7d198
Mateusz Musiorski <mmusiorski@gmail.com>
2023-03-05 10:46:43 +0100

    Add 'setTimeout' to display calculated value after 5 seconds.

--- js/script.js
+++ js/script.js
@@ -19,26 +19,31 @@ const getCurrency = () => {
 // Funkcja pobierająca aktualny kurs danej waluty, przeliczająca ją na złotówki i wyświetlająca wynik końcowy
 const calculatedCurrency = (e) => {
   e.preventDefault();
+  divCalculate.innerHTML = "";
   const currency = getCurrency();
   const url = `http://api.nbp.pl/api/exchangerates/rates/a/${currency}/`;
+
   const ldsRing = document.createElement("div");
   ldsRing.classList.add("lds-ring");
   for (let i = 0; i < 4; i++) {
     const div = document.createElement("div");
     ldsRing.appendChild(div);
   }
-
   divCalculate.appendChild(ldsRing);
-  
+
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
       const currentCurrencyCurse = data.rates[0].mid;
       const calculate = Number(Math.round(`${currentCurrencyCurse * value.value}` + 'e+2') + 'e-2');
-      calculateValue.innerText = calculate;
+      setTimeout(() => {
+        divCalculate.innerHTML = "";
+        const p = document.createElement("p");
+        divCalculate.appendChild(p);
+        p.innerHTML = `It is <span class="sumbit-value">${calculate}</span>PLN.`;
+      }, 5000);
     });
 }
-
 
 select.addEventListener("change", getCurrency);
 form.addEventListener("submit", calculatedCurrency);
