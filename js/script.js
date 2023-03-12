92810c55ed3ec5ffadd18c1893d38b04766e2f93
Mateusz Musiorski <mmusiorski@gmail.com>
2023-03-12 09:41:36 +0100

    Remove unnecessary variables and rename divCalculate to the resultCalculate.

--- js/script.js
+++ js/script.js
@@ -1,18 +1,15 @@
 // Pobieramy potrzebne dane, na których będziemy operować
 
 const form = document.querySelector(".main-form");
-const value = document.querySelector("#value");
+const inputValue = document.querySelector("#value");
 const select = document.querySelector("#main-select");
-const option = document.querySelectorAll("option");
 const btn = document.querySelector("#main-submit");
-const calculateValue = document.querySelector(".sumbit-value");
-const divCalculate = document.querySelector(".main-currency-convertion");
-const para = document.querySelector(".main-currency-convertion-para");
+const resultCalculate = document.querySelector(".main-currency-convertion");
 
 // Funkcja pobierająca aktualny kurs danej waluty, przeliczająca ją na złotówki i wyświetlająca wynik końcowy
 const calculatedCurrency = (e) => {
   e.preventDefault();
-  divCalculate.innerHTML = "";
+  resultCalculate.innerHTML = "";
   const currency = select.options[select.selectedIndex].value;
   const url = `http://api.nbp.pl/api/exchangerates/rates/a/${currency}/`;
 
@@ -22,17 +19,17 @@ const calculatedCurrency = (e) => {
     const div = document.createElement("div");
     ldsRing.appendChild(div);
   }
-  divCalculate.appendChild(ldsRing);
+  resultCalculate.appendChild(ldsRing);
 
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
       const currentCurrencyCurse = data.rates[0].mid;
-      const calculate = Number(Math.round(`${currentCurrencyCurse * value.value}` + 'e+2') + 'e-2');
+      const calculate = Number(Math.round(`${currentCurrencyCurse * inputValue.value}` + 'e+2') + 'e-2');
       setTimeout(() => {
-        divCalculate.innerHTML = "";
+        resultCalculate.innerHTML = "";
         const p = document.createElement("p");
-        divCalculate.appendChild(p);
+        resultCalculate.appendChild(p);
         p.innerHTML = `It is <span class="sumbit-value">${calculate}</span>PLN.`;
       }, 5000);
     });
